3824f126f942242231078a5b12d3791ec3f3d520
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2024-03-17 17:37:49 -0500

    bootstrap with ci not install

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//function based components
+import React from "react";
+
+//Pascale Casing
+function Message() {
+  //JSX, javascript html
+  return <h1>Hello world </h1>;
+}
+
+export default Message;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  //component tree. taking this component and rendering it in an elment root using the reactDOm library
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
